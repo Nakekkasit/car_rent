f90cd187cd1d3b6d7b9ddba816fd1358a370d44b
Nak <nakwondersk@gmail.com>
2024-08-29 19:13:07 +0700

    first commit

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { Fragment, useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+import { CustomFilterProps } from "@/types";
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+  Transition,
+} from "@headlessui/react";
+
+const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const [selected, setSelected] = useState(options[0]); // State for storing the selected option
+
+  return (
+    <div className="w-fit">
+      <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e);
+        }}
+      >
+        <div className="relative w-fit z-10">
+          <ListboxButton className="custom-filter__btn">
+            <span className="block truncate">{selected.title}</span>
+            <Image
+              src="/chevron-up-down.svg"
+              width={20}
+              height={20}
+              className="ml-4 object-contain"
+              alt="chevron_up-down"
+            />
+          </ListboxButton>
+          <Transition
+            as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <ListboxOptions className="custom-filter__options">
+              {options.map((option) => (
+                <ListboxOption
+                  key={option.title}
+                  value={option}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 px-4 ${
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
+                    }`
+                  }
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {option.title}
+                      </span>
+                      {selected ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600"></span>
+                      ) : null}
+                    </>
+                  )}
+                </ListboxOption>
+              ))}
+            </ListboxOptions>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  );
+};
+
+export default CustomFilter;
